1801677180dc2a5c4e50a0f1ea6b62e261392661
Hygue4 <ndanyuzwehygue@gmail.com>
2025-12-12 22:04:12 +0300

    Fix tailwind.config.js to match assignment requirements

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,6 +1,8 @@
+/* eslint-env node */
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false,
   theme: {
     extend: {},
   },
